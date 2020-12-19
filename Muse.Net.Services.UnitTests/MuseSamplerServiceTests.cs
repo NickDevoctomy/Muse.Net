ca8c186cffaec546e343a218dc6e752f5869473e
Nick Pateman <nick@devoctomy.com>
2020-12-19 11:02:15 +0000

    Test coverage.

--- Muse.Net.Services.UnitTests/MuseSamplerServiceTests.cs
+++ Muse.Net.Services.UnitTests/MuseSamplerServiceTests.cs
@@ -7,7 +7,7 @@ namespace Muse.Net.Services.UnitTests
     public class MuseSamplerServiceTests
     {
         [Fact]
-        public void GivenChannel_And10Values_WhenSample_ThenSamplesStored_AndGetSampleCountForPreviousSecondReturns10()
+        public void GivenSample10Values_WhenGetSampleCountForLastSecond_Then10Returned()
         {
             // Arrange
             var config = new MuseSamplerServiceConfiguration
@@ -17,21 +17,21 @@ namespace Muse.Net.Services.UnitTests
             var sut = new MuseSamplerService(config);
             var channel = Channel.EEG_AF7;
             var values = new float[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            // Act
             sut.Sample(
                 channel,
                 values);
 
-            // Assert
-            Assert.Equal(10,
-                sut.GetSampleCount(
+            // Act
+            var result = sut.GetSampleCount(
                     channel,
-                    new System.TimeSpan(0, 0, 1)));
+                    new System.TimeSpan(0, 0, 1));
+
+            // Assert
+            Assert.Equal(10, result);
         }
 
         [Fact]
-        public async Task GivenChannel_And10Values_WhenSample_ThenSamplesStored_AndWait2Seconds_GetSampleCountForPreviousSecondReturns0()
+        public async Task GivenSample10Values_AndWait2Seconds_WhenGetSampleCountForLastSecond_Then0Returned()
         {
             // Arrange
             var config = new MuseSamplerServiceConfiguration
@@ -41,18 +41,72 @@ namespace Muse.Net.Services.UnitTests
             var sut = new MuseSamplerService(config);
             var channel = Channel.EEG_AF7;
             var values = new float[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            // Act
             sut.Sample(
                 channel,
                 values);
             await Task.Delay(2000);
 
-            // Assert
-            Assert.Equal(0,
-                sut.GetSampleCount(
+            // Act
+            var result = sut.GetSampleCount(
                     channel,
-                    new System.TimeSpan(0, 0, 1)));
+                    new System.TimeSpan(0, 0, 1));
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void GivenSample10Values_WheTryGetSamplesForLastSecond_ThenAllSamplesReturned()
+        {
+            // Arrange
+            var config = new MuseSamplerServiceConfiguration
+            {
+                SamplePeriod = new System.TimeSpan(0, 0, 1)
+            };
+            var sut = new MuseSamplerService(config);
+            var channel = Channel.EEG_AF7;
+            var values = new float[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            sut.Sample(
+                channel,
+                values);
+
+            // Act
+            var gotSamples = sut.TryGetSamples(
+                    channel,
+                    new System.TimeSpan(0, 0, 1),
+                    out var samples);
+
+            // Assert
+            Assert.True(gotSamples);
+            Assert.Equal(10, samples.Length);
+            Assert.Equal(values, samples);
+        }
+
+        [Fact]
+        public async Task GivenSample10Values_AndWait2Seconds_WheTryGetSamplesForLastSecond_ThenNoSamplesReturned()
+        {
+            // Arrange
+            var config = new MuseSamplerServiceConfiguration
+            {
+                SamplePeriod = new System.TimeSpan(0, 0, 1)
+            };
+            var sut = new MuseSamplerService(config);
+            var channel = Channel.EEG_AF7;
+            var values = new float[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            sut.Sample(
+                channel,
+                values);
+            await Task.Delay(2000);
+
+            // Act
+            var gotSamples = sut.TryGetSamples(
+                    channel,
+                    new System.TimeSpan(0, 0, 1),
+                    out var samples);
+
+            // Assert
+            Assert.True(gotSamples);
+            Assert.Empty(samples);
         }
     }
 }
